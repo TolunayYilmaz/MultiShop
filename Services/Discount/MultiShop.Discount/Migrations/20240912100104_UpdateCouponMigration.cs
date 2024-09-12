29e2cfdd73f32cff4a469218d6d0ff3ff1c4188d
Tolunay <88236341+TolunayYilmaz@users.noreply.github.com>
2024-09-12 13:22:41 +0300

    Added Discount Microservice

---
+++ Services/Discount/MultiShop.Discount/Migrations/20240912100104_UpdateCouponMigration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MultiShop.Discount.Migrations
+{
+    public partial class UpdateCouponMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "isActive",
+                table: "Coupons",
+                newName: "IsActive");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsActive",
+                table: "Coupons",
+                newName: "isActive");
+        }
+    }
+}
