26ca0e7ad8799c8866f46399c71b7b92492ac269
Tolunay <88236341+TolunayYilmaz@users.noreply.github.com>
2024-09-13 14:52:42 +0300

    Updated Mediator Design Pattern

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/Mediator/Handlers/OrderingHandlers/CreateOrderingQueryHandler.cs
@@ -0,0 +1,65 @@
+﻿
+using MediatR;
+using MultiShop.Order.Application.Features.Mediator.Commands.OrderingCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entitites;
+
+namespace MultiShop.Order.Application.Features.Mediator.Handlers.OrderingHandlers
+{
+    public class CreateOrderingQueryHandler : IRequestHandler<CreateOrderingCommand>
+    {
+        private readonly IRepository<Ordering> _repository;
+
+        public CreateOrderingQueryHandler(IRepository<Ordering> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(CreateOrderingCommand request, CancellationToken cancellationToken)
+        {
+            await _repository.CreateAsync(new Ordering
+            {
+                OrderDate = request.OrderDate,
+                TotalPrice = request.TotalPrice,    
+                UserId = request.UserId  
+
+            });
+        }
+    }
+    
+    public class RemoveOrderingQueryHandler : IRequestHandler<RemoveOrderingCommand>
+    {
+        private readonly IRepository<Ordering> _repository;
+
+        public RemoveOrderingQueryHandler(IRepository<Ordering> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(RemoveOrderingCommand request, CancellationToken cancellationToken)
+        {
+            var value = await _repository.GetByIdAsync(request.Id);
+            await _repository.DeleteAsync(value);
+
+        }
+    }  
+    
+    public class UpdateOrderingQueryHandler : IRequestHandler<UpdateOrderingCommand>
+    {
+        private readonly IRepository<Ordering> _repository;
+
+        public UpdateOrderingQueryHandler(IRepository<Ordering> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateOrderingCommand request, CancellationToken cancellationToken)
+        {
+            var value = await _repository.GetByIdAsync(request.OrderingId);
+            value.OrderDate = request.OrderDate;
+            value.TotalPrice = request.TotalPrice;
+            value.UserId = request.UserId;
+            await _repository.UpdateAsync(value);
+        }
+    }
+}
