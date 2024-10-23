dae278d7917c0a2258600f993231155fed1953c7
Tolunay <88236341+TolunayYilmaz@users.noreply.github.com>
2024-10-23 22:44:08 +0300

    Updated Comment Pages_2

--- Frontends/MultiShop.WebUI/Controllers/ProductListController.cs
+++ Frontends/MultiShop.WebUI/Controllers/ProductListController.cs
@@ -1,10 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CommentDtos;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace MultiShop.WebUI.Controllers
 {
     public class ProductListController : Controller
     {
-        public IActionResult Index(string id)
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public ProductListController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+		public IActionResult Index(string id)
         {
             ViewBag.i=id;   
             return View();
@@ -14,6 +23,31 @@ namespace MultiShop.WebUI.Controllers
         {
             ViewBag.x=id;
             return View();
+        }
+        [HttpGet]
+        public PartialViewResult AddComment()
+        {
+
+			return PartialView();
+		}
+        [HttpPost]
+        public async Task<IActionResult> AddComment(CreateCommentDto createCommentDto)
+        {
+            createCommentDto.ImageUrl = "Test";
+            createCommentDto.Rating = 1;
+            createCommentDto.CreatedDate =DateTime.Parse(DateTime.Now.ToShortDateString());
+            createCommentDto.Status = false;
+            createCommentDto.ProductId = "SabununId";///güncellencek 
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createCommentDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7261/api/Comments", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Default");
+            }
+            return View();
+            
         }
     }
 }
