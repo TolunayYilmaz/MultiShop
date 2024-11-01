2cb38f820e37d3154b0b8a81f93b0651a4e125ba
Tolunay <88236341+TolunayYilmaz@users.noreply.github.com>
2024-11-01 22:52:12 +0300

    Added Order Page

--- Frontends/MultiShop.WebUI/Controllers/DiscountController.cs
+++ Frontends/MultiShop.WebUI/Controllers/DiscountController.cs
@@ -6,25 +6,35 @@ namespace MultiShop.WebUI.Controllers
 {
     public class DiscountController : Controller
     {
-        private IDiscountService _discountService;
-       
-        public DiscountController(IDiscountService discountService )
+        private readonly IDiscountService _discountService;
+        private readonly IBasketService _basketService;
+
+        public DiscountController(IDiscountService discountService, IBasketService basketService)
         {
             _discountService = discountService;
-         
+            _basketService = basketService;
         }
         [HttpGet]
         public PartialViewResult ConfirmDiscountCoupon()
         {
-            
+
             return PartialView();
-        }  
+        }
         [HttpPost]
-        public IActionResult ConfirmDiscountCoupon(string code)
+        public async Task<IActionResult> ConfirmDiscountCoupon(string code)
         {
+
+            var value =await _discountService.GetDiscountCodeDetailByCodeAsync(code);
+            var basketValues =await _basketService.GetBasket();
+            var totalPriceWithTax = basketValues.TotalPrice + (basketValues.TotalPrice / 100) * 10;
+            var totalNewPriceWithDiscount = totalPriceWithTax - (totalPriceWithTax/100*value.Rate);
            
-            var value=_discountService.GetDiscountCodeDetailByCodeAsync(code);
-            return View(value);
+            //var value = await _basketService.GetBasket();
+            //ViewBag.totalPrice = value.TotalPrice;
+            //var totalPriceWithTax = value.TotalPrice + (value.TotalPrice / 100) * 10;
+            //var tax = (value.TotalPrice / 100) * 10;
+            //ViewBag.tax = tax;
+            return RedirectToAction("Index","ShopingCart",new {code=code,discountRate=value.Rate, totalNewPriceWithDiscount= totalNewPriceWithDiscount });
         }
     }
 }
