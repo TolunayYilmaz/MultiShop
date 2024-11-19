a67024ba4ca15bdf174bb8bf8d9eb0181a608d49
Tolunay <88236341+TolunayYilmaz@users.noreply.github.com>
2024-11-19 14:46:35 +0300

    Added GoogleCloudStorage by ImageServices

---
+++ Services/Images/MultiShop.Images.WebUI/Controllers/DefaultController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.Images.WebUI.DAL.Entities;
+using MultiShop.Images.WebUI.Services;
+
+
+namespace MultiShop.Images.WebUI.Controllers
+{
+    public class DefaultController : Controller
+    {
+
+        private readonly ICloudStorageService _cloudStorageService;
+
+        public DefaultController(ICloudStorageService cloudStorageService)
+        {
+
+            _cloudStorageService = cloudStorageService;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ImageDrive ımageDrive)
+        {
+            if (ModelState.IsValid)
+            {
+                if (ımageDrive.Photo != null)
+                {
+                    ımageDrive.SavedFileName = GenerateFileNameToSave(ımageDrive.Photo.FileName);
+                    ımageDrive.SavedUrl = await _cloudStorageService.UploadFileAsync(ımageDrive.Photo, ımageDrive.SavedFileName);
+                }
+
+                return RedirectToAction("Index", "Default");
+            }
+            return View(ımageDrive);
+        }
+
+        private string? GenerateFileNameToSave(string incomingFileName)
+        {
+            var fileName = Path.GetFileNameWithoutExtension(incomingFileName);
+            var extension = Path.GetExtension(incomingFileName);
+            return $"{fileName}-{DateTime.Now.ToUniversalTime().ToString("yyyyMMddHHmmss")}{extension}";
+        }
+
+
+
+    }
+}
---
+++ Services/Images/MultiShop.Images/Controllers/GoogleDriveImageUploadController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.Images.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class GoogleDriveImageUploadController : ControllerBase
+    {
+    }
+}
